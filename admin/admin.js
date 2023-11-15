dd7384f6a863846316a85590f9ee820daf2e6100
anhpp0910 <phuonganhphanhhh@gmail.com>
2023-11-15 17:08:26 +0700

    Fix

--- admin/admin.js
+++ admin/admin.js
@@ -25,6 +25,8 @@ window.deletePhone = (id) => {
     .then(function (res) {
       // gọi lại api lấy ds sp mới nhất từ server sau khi xóa thành công
       getPhonesList();
+      // set sortByPrice về lại default
+      document.getElementById("sortByPrice").value = "default";
     })
     .catch(function (err) {});
 };
@@ -38,6 +40,10 @@ window.createPhone = () => {
   })
     .then(function (res) {
       getPhonesList();
+      // clear field input
+      clearFields();
+      // set sortByPrice về lại default
+      document.getElementById("sortByPrice").value = "default";
       // tắt modal sau khi thêm thành công
     })
     .catch(function (err) {});
@@ -63,6 +69,11 @@ window.editPhone = (id) => {
       document.getElementById("image").value = phone.img;
       document.getElementById("desc").value = phone.desc;
       document.getElementById("brand").value = phone.brand;
+      // set sortByPrice về lại default
+      document.getElementById("sortByPrice").value = "default";
+      // Enable button Update, disable button Add
+      document.getElementById("btnAdd").disabled = true;
+      document.getElementById("btnUpdate").disabled = false;
     })
     .catch(function (err) {});
 };
@@ -78,6 +89,61 @@ window.updatePhone = () => {
     .then(function (res) {
       // sau khi cập nhật thành công
       getPhonesList();
+      // clear field input, enable button Add, disable button Update
+      clearFields();
+      document.getElementById("btnAdd").disabled = false;
+      document.getElementById("btnUpdate").disabled = true;
+      // set sortByPrice về lại default
+      document.getElementById("sortByPrice").value = "default";
     })
     .catch(function (err) {});
+};
+
+// Search by name
+document.getElementById("btnSearch").onclick = () => {
+  axios({
+    url: "https://653122f94d4c2e3f333c72a3.mockapi.io/phones",
+    method: "GET",
+  })
+    .then(function (res) {
+      var phoneArr = res.data;
+      var searchName = document.getElementById("searchName").value;
+      var searchResult = phoneArr.filter(
+        (phone) => phone.name.toLowerCase().includes(searchName) === true
+      );
+      renderPhoneList(searchResult);
+      // set sortByPrice về lại default
+      document.getElementById("sortByPrice").value = "default";
+    })
+    .catch(function (err) {
+      console.log("err", err);
+    });
+};
+
+// Sort by Price
+document.getElementById("sortByPrice").onchange = () => {
+  axios({
+    url: "https://653122f94d4c2e3f333c72a3.mockapi.io/phones",
+    method: "GET",
+  })
+    .then(function (res) {
+      var phoneArr = res.data;
+      var sortValue = document.getElementById("sortByPrice").value;
+      switch (sortValue) {
+        case "lowToHigh":
+          var lowToHighArr = phoneArr.sort((a, b) => a.price * 1 - b.price * 1);
+          renderPhoneList(lowToHighArr);
+          break;
+        case "highToLow":
+          var highToLowArr = phoneArr.sort((a, b) => b.price * 1 - a.price * 1);
+          renderPhoneList(highToLowArr);
+          break;
+        default:
+          getPhonesList();
+          break;
+      }
+    })
+    .catch(function (err) {
+      console.log("err", err);
+    });
 };
--- admin/controller.js
+++ admin/controller.js
@@ -9,13 +9,13 @@ function renderPhoneList(phoneArr) {
                             <td>${phone.screen}</td>
                             <td>${phone.backCamera}</td>
                             <td>${phone.frontCamera}</td>
-                            <td>${phone.img}</td>
+                            <td class="phoneImg"><img src="${phone.img}"></td>
                             <td>${phone.desc}</td>
                             <td>${phone.brand}</td>
                             <td>
-                                <button onclick='deletePhone(${phone.id})' class="btn btn-danger">Xóa</button>
+                                <button onclick='deletePhone(${phone.id})' class="btn btn-danger">Delete</button>
                                 <button data-bs-toggle="modal"
-                                data-bs-target="#myModal" onclick='editPhone(${phone.id})' class="btn btn-warning">Sửa</button>
+                                data-bs-target="#myModal" onclick='editPhone(${phone.id})' class="btn btn-warning mt-2">Edit</button>
                             </td>
                         </tr>`;
     contentHTML += trString;
@@ -42,4 +42,24 @@ function getDataForm() {
     desc: moTa,
     brand: thuongHieu,
   };
+}
+
+// Xoá tất cả value của field input
+function clearFields() {
+  document.getElementById("phoneName").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("screen").value = "";
+  document.getElementById("backCamera").value = "";
+  document.getElementById("frontCamera").value = "";
+  document.getElementById("image").value = "";
+  document.getElementById("desc").value = "";
+  document.getElementById("brand").value = "";
+}
+
+// Xoá tất cả input, error tip, enable/disable button Add/Update khi nhấn Close modal
+function closeModal() {
+  clearFields();
+  // clearErrors();
+  document.getElementById("btnAdd").disabled = false;
+  document.getElementById("btnUpdate").disabled = true;
 }
